85654ff040163bcdc9a25bc8e10d98d6350181ef
Karim <Karmo>
2019-08-16 10:13:47 +0200

    add file for eventIDs

---
+++ PPConstants.lua
@@ -0,0 +1,11 @@
+local evnets = {}
+
+evnets.test = "test"
+evnets.attack = "eaten"
+evnets.whoAreYou = "preyOrPreditor"
+evnets.Fox = "fox"
+evnets.Rabbit = "rabbit"
+evnets.consumed = "mums"
+
+
+return evnets
